9e7758d3887e53413a659fac8bb691bc8c5c4b3f
WeiJianYe <w15127177271@outlook.com>
2025-10-29 20:32:28 +0800

    解决主题配置文件里图片不显示问题，更新主页

--- docs/.vuepress/config/themeConfig.ts
+++ docs/.vuepress/config/themeConfig.ts
@@ -1,7 +1,7 @@
 import { VdoingThemeConfig } from "vuepress-theme-vdoing/types";
 
 export default <VdoingThemeConfig>{
-  logo: '![](https://raw.githubusercontent.com/Wcacciatori/imgs/main/imgs/20251029200734947.png)',
+  logo: 'https://raw.githubusercontent.com/Wcacciatori/imgs/main/imgs/20251029200734947.png',
   nav: [
     { text: '首页', link: '/' },
     { text: '成电', items: [
@@ -23,15 +23,12 @@ export default <VdoingThemeConfig>{
   // docsDir: 'docs',
   // editLinks: true,
   // editLinkText: '编辑此页',
-  bodyBgImg:'![](https://raw.githubusercontent.com/Wcacciatori/imgs/main/imgs/20251029202245101.jpg)',
-  bodyBgImgOpacity: 0.2,
+  bodyBgImg:'https://raw.githubusercontent.com/Wcacciatori/imgs/main/imgs/20251029202245101.jpg',
+  bodyBgImgOpacity: 0.5,
   lastUpdated: '上次更新', 
 
   // 页脚
   footer: {
-    createYear: 2022,
-    copyrightInfo: `<a href='https://beian.miit.gov.cn'>粤ICP备2022067627号-1</a>  
-      <a href='http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44011302003646'>粤公网安备 44011302003646号</a>
-      `
+    createYear: 2022
   },
 }
