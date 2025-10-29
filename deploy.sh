87ba713246a090dc209f72b5db8d422c61dccc68
WeiJianYe <w15127177271@outlook.com>
2025-10-29 14:33:26 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,17 @@
 set -e
 
 # 生成静态文件
-# npm run docs:build
+npm run docs:build
 
 # 进入生成的文件夹
-# cd docs/.vuepress/dist
+cd docs/.vuepress/dist
 
-# git init
+# 添加 git 提交信息
+git init
 git add .
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:Wcacciatori/Blog.git master:gh-pages
-git push 
+# 推送到 gh-pages 分支
+git push -f git@github.com:Wcacciatori/Blog.git master:gh-pages
 
 cd -
